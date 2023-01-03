fe4a716ffd42c2d5078c4f3c19982695681ff1fd
lcbranco <luyzcastelobranco@gmail.com>
2023-01-03 15:03:10 -0300

    readbility optimization

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
 function main() {
-  console.log(localStorage)
   const chekboxes = document.querySelectorAll('#is-task-done');
   
   chekboxes.forEach((checkbox, index) => {
     const checkboxKeyInStorage = `checkbox: ${index + 1}`;
     const checkboxValueInStorage = localStorage.getItem(checkboxKeyInStorage);
-    let checkboxValue;
 
-    if(checkboxValueInStorage === 'true') checkboxValue = true;
-    else checkboxValue = false;
-    checkbox.checked = checkboxValue;
+    checkbox.checked = checkboxValueInStorage === 'true' ? true : false;
 
     checkbox.addEventListener('change', () => localStorage.setItem(checkboxKeyInStorage, checkbox.checked));
   })
