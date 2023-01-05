0aeb88291c412da1f4b8377bb5efa5f7e4349c1b
lcbranco <luyzcastelobranco@gmail.com>
2023-01-05 08:50:57 -0300

    useless change to how the roadmaps lists are made

--- main.js
+++ main.js
@@ -1,14 +1,84 @@
 function main() {
-  const chekboxes = document.querySelectorAll('#is-task-done');
+  const roadmaps = [
+    {
+      linkHref: 'https://www.roadmap.sh/javascript',
+      linkText: 'Javascript Roadmap',
+      inputId: 'Javascript'
+    },
+    {
+      linkHref: 'https://www.roadmap.sh/frontend',
+      linkText: 'Frontend Roadmap',
+      inputId: 'Frontend'
+    },
+  ]
+  const listOfRoadmapsInHTML = document.querySelector('#roadmap-list-js');
+
+  roadmaps.forEach(roadmap => listOfRoadmapsInHTML.appendChild(createLiElements(roadmap.linkHref, roadmap.linkText, roadmap.inputId)));
+};
+
+function createLiElements(linkHref, linkText, inputId) {
+  const li = document.createElement('li');
+
+  const h2 = createHeadingElement(2);
+  const link = createLinkElement(linkHref, linkText);
   
-  chekboxes.forEach((checkbox, index) => {
-    const checkboxKeyInStorage = `checkbox: ${index + 1}`;
-    const checkboxValueInStorage = localStorage.getItem(checkboxKeyInStorage);
+  h2.appendChild(link);
+  
+  const div = createDivElement();
+  
+  const h3 = createHeadingElement(3);
+  h3.innerText = "have you done this roadmap's tasks?";
 
-    checkbox.checked = checkboxValueInStorage === 'true' ? true : false;
+  const input = createInputElement(inputId);
+  
+  div.appendChild(h3)
+  div.appendChild(input)
+  
+  li.appendChild(h2);
+  li.appendChild(div);
 
-    checkbox.addEventListener('change', () => localStorage.setItem(checkboxKeyInStorage, checkbox.checked));
-  })
+  return li;
 }
+
+function createHeadingElement(headingNum) {
+  if(headingNum > 0 && headingNum < 7) {
+    const heading = document.createElement(`h${headingNum}`);
+
+    return heading;
+  }
+
+  return 'heading num given is invalid';
+}
+
+function createLinkElement(link, linkText) {
+  const linkTag = document.createElement('a')
+  linkTag.setAttribute('target', 'blank')
+  linkTag.setAttribute('href', link);
+  linkTag.innerText = linkText;
+
+  return linkTag;
+}
+
+function createDivElement() {
+  const div = document.createElement('div');
+  div.setAttribute('class', 'question');
+
+  return div;
+}
+
+function createInputElement(inputId) {
+  const input = document.createElement('input');
+  input.setAttribute('type', 'checkbox');
+  input.setAttribute('id', 'is-task-done');
+
+  const checkboxKeyInStorage = inputId;
+  const checkboxValueInStorage = localStorage.getItem(checkboxKeyInStorage);
+
+  input.checked = checkboxValueInStorage === 'true' ? true : false;
+
+  input.addEventListener('change', () => localStorage.setItem(checkboxKeyInStorage, input.checked));
+
+  return input;
+};
 
 main();
